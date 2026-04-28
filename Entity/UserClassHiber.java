d44d571257cdfc57b87ebebce9abe444dd23df9f
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-28 09:53:21 +0530

    Update UserClassHiber.java

--- Entity/UserClassHiber.java
+++ Entity/UserClassHiber.java
@@ -1,1 +1,28 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class UserClassHiber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+
+    UserClassHiber() {
+
+    }
+
+    UserClassHiber(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserClassHiber(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
