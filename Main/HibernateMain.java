431b2ea3e9349e687111a5e521e995f31863e52b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-29 09:12:08 +0530

    Update HibernateMain.java

--- Main/HibernateMain.java
+++ Main/HibernateMain.java
@@ -1,1 +1,17 @@
+import org.hibernate.Session;
 
+public class HibernateMain {
+    public static void main(String[] args) {
+        Session session = hibernateUtil.getSession();
+        try {
+            UserClassHiber user = new
+                    UserClassHiber("Rahul", "rahul@gmail.com");
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+            System.out.println("User saved: " + user.getId());
+        } catch(Exception e) {
+            e.printStackTrace();
+        } 
+    }
+}
