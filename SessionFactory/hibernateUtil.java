c0cd796313a39f79db6a91d905c3b997722ec5b8
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-30 09:24:10 +0530

    Update hibernateUtil.java

--- SessionFactory/hibernateUtil.java
+++ SessionFactory/hibernateUtil.java
@@ -1,1 +1,18 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class hibernateUtil {
+    public static SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(UserClassHiber.class)
+                    .buildSessionFactory();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
