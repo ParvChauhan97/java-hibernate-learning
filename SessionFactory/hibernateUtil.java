71d90cec6eb77dab7db3b3d1961447fef7e956ab
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-30 09:25:16 +0530

    Add close method to SessionFactory

--- SessionFactory/hibernateUtil.java
+++ SessionFactory/hibernateUtil.java
@@ -18,4 +18,7 @@ public class hibernateUtil {
     public static Session getSession() {
         return sessionFactory.getCurrentSession();
     }
+     public static void close() {
+        sessionFactory.close();
+    }
 }
